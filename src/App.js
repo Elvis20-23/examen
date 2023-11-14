c4568dd3f79882a61d381b974c07c79a0d902ef6
Elvis20-23 <137233039+Elvis20-23@users.noreply.github.com>
2023-11-14 17:29:00 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       
       <h1>INSTITUTO DE EDUCACIÓN SUPERIOR PÚBLICO "HONORIO DELGADO ESPINOZA"</h1>
       <h3>Alumno: Erick Saúl Checya Aragón</h3>
-      <h3>Fecha: 14/11/2023      Turno: noche</h3>
+      <h3>Fecha: 14/11/2023   /   Turno: noche</h3>
       
     </div>
   );
